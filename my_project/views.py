cee738c9a36fc2d987e0a35a8e769bf6e0adc3ce
Emma Kopf <emmakopf@gmail.com>
2017-02-12 17:31:23 -0500

    Update views.py

--- my_project/views.py
+++ my_project/views.py
@@ -8,3 +8,36 @@ class HomePageView(TemplateView):
     def get(self, request, **kwargs):
         return render(request, 'index.html', context=None)
 
+class BookSellView(ListView):
+    model = sell_book
+    def get(self, request, *args, **kwargs):
+        book = get_object_or_404(sell_book, pk = kwargs['book-id'])
+        book = model_to_dict(book)
+
+
+class BookBuyView(ListView):
+
+class SellerView(ListView):
+
+class BuyerView(ListView):
+
+
+
+class CarSellView(ListView):
+    model = car_to_sell
+    
+    def get(self, request, *args, **kwargs):
+        car = get_object_or_404(car_to_sell, pk=kwargs['car_id'])
+        car = model_to_dict(car)
+        return _success(car, 'car_to_sell', 200)
+    
+    def post(self, request, *args, **kwargs):
+        def post(self, request, *args, **kwargs):
+            data = request.body.decode('utf-8')
+            data_dict = json.loads(data)
+            form = CarSellForm(data_dict)
+            if form.is_valid() :
+                form.save()
+                return HttpResponse('Success')
+            else :
+                return HttpResponse('Bad Post Request')
