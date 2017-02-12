161ebb0fb0cbb727e7bc488d3d9337d671867c27
Emma Kopf <emmakopf@gmail.com>
2017-02-12 17:18:59 -0500

    Update urls.py

--- my_project/urls.py
+++ my_project/urls.py
@@ -5,4 +5,12 @@ from .views import *
 
 index = [url(r'^$', views.index),]
 
-book = [
+book = [url(r'book/sale/(?P<book_id>[0-9]+)', BookSellView.as_view()),
+       url(r'book/buy/(?P<book_id>[0-9]+)', BookBuyView.as_view()),
+       url(r'user/seller/(?P<user_id>[0-9]+)', SellerView.as_view()),
+       url(r'user/buyer/(?P<user_id>[0-9]+)', BuyerView.as_view()),
+       url(r'create/user/seller', SellerView.as_view()),
+       url(r'create/user/buyer', BuyerView.as_view()),
+       ]
+
+urlpatterns = index + car
