88c580a9d0768721f1d6b5979024202ef04ea8e3
Emma Kopf <emmakopf@gmail.com>
2017-02-12 17:57:43 -0500

    view files

--- my_project/models.py
+++ my_project/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+class book(models.Model):
+    title = models.CharField(max_length = 200)
+    author = models.CharField(max_length = 200)
+    publisher = models.CharField(max_length = 200)
+    pub_date = models.DateTimeField('date published')
+    isbn_num = models.charField(max_length = 10)
+
+class user(models.Model):
+    first_name = models.CharField(max_length=20)
+    last_name  = models.CharField(max_length=20)
+    user_name = models.CharField(max_length=30)
+    password = models.CharField(max_length=30)
+
+class sell_book(book):
+    price_to_sell = models.IntegerField(default=0)
+
+class seller(user):
+    book_to_sell = models.ManyToManyField(sell_book)
+
+class buy_book(car):
+    price_to_offer = models.IntegerField(default=0)
+
+class buyer(user):
+    book_to_buy = models.ManyToManyField(buy_book)
+
+class inventory(book):
+
+class book_review(book):
--- my_project/views.py
+++ my_project/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.shortcuts import render, get_object_or_404
 
 import json
 
@@ -8,3 +9,47 @@ class HomePageView(TemplateView):
     def get(self, request, **kwargs):
         return render(request, 'index.html', context=None)
 
+class BookSellView(ListView):
+    model = sell_book
+    def get(self, request, *args, **kwargs):
+        book = get_object_or_404(sell_book, pk = kwargs['book-id'])
+        book = model_to_dict(book)
+        return JsonResponse(model_name : data_dict)
+    def post(self, request, *args, **kwargs):
+        data = request.body.decode('utf-8')
+        data_dict = json.loads(data)
+        return HttpResponse('Good')
+
+
+class BookBuyView(ListView):
+    model = buy_book
+    def get(self, request, *args, **kwargs):
+        book = get_object_or_404(buy_book, pk = kwargs['book-id'])
+        book = model_to_dict(book)
+        return JsonResponse(model_name : data_dict)
+    def post(self, request, *args, **kwargs):
+        data = request.body.decode('utf-8')
+        data_dict = json.loads(data)
+        return HttpResponse('Good')
+
+
+class SellerView(ListView):
+    model = seller
+    def get(self, request, *args, **kwargs):
+        book = get_object_or_404(seller, pk = kwargs['book-id'])
+        book = model_to_dict(book)
+        return JsonResponse(model_name : data_dict)
+    def post(self, request, *args, **kwargs):
+        data = request.body.decode('utf-8')
+        data_dict = json.loads(data)
+
+
+class BuyerView(ListView):
+    model = buyer
+    def get(self, request, *args, **kwargs):
+        book = get_object_or_404(buyer, pk = kwargs['book-id'])
+        book = model_to_dict(book)
+        return JsonResponse(model_name : data_dict)
+    def post(self, request, *args, **kwargs):
+        data = request.body.decode('utf-8')
+        data_dict = json.loads(data)
