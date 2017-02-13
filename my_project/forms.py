e95186480ca85f1c6e9b9fee262b90357dc5a10a
aar0n-aguh0b <aaronaguhob@gmail.com>
2017-02-13 15:26:18 -0500

    Added BookForm to form.py

--- my_project/forms.py
+++ my_project/forms.py
@@ -1,7 +1,12 @@
 from django import forms
-from .models import user
+from .models import user, book
 
 class UserForm(forms.ModelForm):
     class Meta:
         model = user
         fields = ('first_name', 'last_name', 'user_name','password')
+
+class BookForm(forms.ModelForm):
+    class Meta:
+        model = book
+        fields = ('title', 'author', 'isbn_num')
