f7493bfc0826b041e9512bdcc356bec61b718553
John <jpm6zv@virginia.edu>
2017-02-24 11:19:34 -0500

    Working Get/Post Api

--- my_project/urls.py
+++ my_project/urls.py
@@ -1,30 +1,10 @@
 from django.conf.urls import url
-from my_project import views
 from . import views
-from .views import *
 
-index = [url(r'^$', views.index),]
 
-book = [#url(r'book/sale/(?P<book_id>[0-9]+)', BookSellView.as_view()),
-        #url(r'book/buy/(?P<book_id>[0-9]+)', BookBuyView.as_view()),
-        #url(r'user/seller/(?P<user_id>[0-9]+)', SellerView.as_view()),
-        #url(r'user/buyer/(?P<user_id>[0-9]+)', BuyerView.as_view()),
-        #url(r'book/add/$', views.BookCreate.as_view(), name='add-book'),
-        #url(r'create/user/seller', SellerView.as_view()),
-        #url(r'create/user/buyer', BuyerView.as_view()),
-       ]
-views = [url(r'^createUser/', views.createUser),
-         url(r'^user_display/(?P<user_id>[\w|\W]+)/$', views.viewUser),
-         url(r'^user_display/', views.UserView),
-         #url(r'^user_display/(?P<user>[\w|\W]+)/$', views.viewUser),
-         url(r'^removeUser/(?P<user_id>[\w|\W]+)/$', views.removeUser),
-         url(r'^book_display/(?P<book_id>[\w|\W]+)/$', views.viewBook),
-         url(r'^book_display/', views.BookView),
-         #url(r'^book_display/(?P<book_id>[\w|\W]+)/$', views.viewBook),
-         url(r'^createBook/', views.createBook),
-        url(r'^removeBook/(?P<book_id>[\w|\W]+)/$', views.removeBook),
-         url(r'^user_edit/(?P<user_id>[\w|\W]+)/$', views.editUser),
-         url(r'^book_edit/(?P<book_id>[\w|\W]+)/$', views.editBook)]
-#user = [url(r'user/add/$', views.UserCreate.as_view(), name='add-user'),
-# ]
-urlpatterns = index + book + views
+urlpatterns = [url(r'^createUser/', views.createUser),
+               url(r'^createBook/', views.createBook),
+               url(r'^user_display/(?P<user_id>[\w|\W]+)/$', views.viewUser),
+               url(r'^removeUser/(?P<user_id>[\w|\W]+)/$', views.removeUser),
+               url(r'^book_display/(?P<book_id>[\w|\W]+)/$', views.viewBook),
+               url(r'^removeBook/(?P<book_id>[\w|\W]+)/$', views.removeBook),]
--- my_project/views.py
+++ my_project/views.py
@@ -1,184 +1,93 @@
 from django.http import JsonResponse
 from .forms import UserForm, BookForm
+from django.forms import model_to_dict
 from .models import user, book
-from django.shortcuts import render
-import json
-
-# Create your views here.
-def index(request):
-    return render(request, 'index.html', context=None)
-
-def UserView(request):
-    users = user.objects.all()
-    UserIDs = []
-    for usr in users:
-        if (usr.id not in UserIDs):
-            UserIDs.append(usr.id)
-    return render(request, 'user_display.html', {'Users': UserIDs})
+from django.shortcuts import render, HttpResponse
 
 def viewUser(request, user_id):
-    if user.objects.filter(id = user_id):
-        theUser = user.objects.filter(id = user_id)
-        theUser = theUser.get()
-        json = {}
-        json ['username'] = theUser.user_name
-        json ['first name'] = theUser.first_name
-        json ['last name'] = theUser.last_name
-        json ['password'] = theUser.password
-        json ['ID'] = theUser.id
-        return JsonResponse(json)
+    if user.objects.filter(id=user_id):
+        theUser = user.objects.get(pk=user_id)
     else:
-        return JsonResponse({
-                            "status":"error",
-                            "message": "No user was found with that ID",
-                            "data": None
-                            })
+        return JsonResponse({'status': False, 'message': "No user was found with that ID", 'data': None})
+    #Display User
+    if(request.method == "GET"):
+        if user.objects.filter(id=user_id):
+            user_dict = model_to_dict(theUser, exclude=['bag'])
+            return JsonResponse({'status': True, 'resp': user_dict})
+    # Edit User
+    # talk to TA about how to have no required fields for this
+    elif(request.method == "POST"):
+        form = UserForm(request.POST, instance=theUser)
+        if (form.is_valid()):
+            form.save()
+            theUser = user.objects.get(pk=user_id)
+            user_dict = model_to_dict(theUser, exclude=['bag'])
+            return JsonResponse({'status': True, 'resp': user_dict})
+        else:
+            return JsonResponse({'status': False, 'resp': 'invalid input'})
 
+    return JsonResponse({'status': False, 'resp': 'URL only handles GET and POST requests'})
 
-def BookView(request):
-    books = book.objects.all()
-    BookIDs = []
-    for bk in books:
-        if (bk.id not in BookIDs):
-            BookIDs.append(bk.id)
-    return render(request, 'book_display.html', {'Books': BookIDs})
 
 def viewBook(request, book_id):
-    if book.objects.filter(id = book_id):
-        theBook = book.objects.filter(id = book_id)
-        theBook = theBook.get()
-        json = {}
-        json['title'] = theBook.title
-        json['author'] = theBook.author
-        json['publisher'] = theBook.publisher
-        json['publication date'] = theBook.pub_date
-        json['isbn'] = theBook.isbn_num
-        json['price'] = theBook.price
-        json['ID'] = theBook.id
-        return JsonResponse(json)
+    if book.objects.filter(id=book_id):
+        theBook = book.objects.get(pk=book_id)
     else:
-        return JsonResponse({
-                            "status":"error",
-                            "message":"No book was found with that ID",
-                            "data": None
-                            })
+        return JsonResponse({'status': False, 'message': "No book was found with that ID", 'data': None})
+    # Display Book
+    if (request.method == "GET"):
+        if book.objects.filter(id=book_id):
+            book_dict = model_to_dict(theBook)
+            return JsonResponse({'status': True, 'resp': book_dict})
+    # Edit Book
+    # talk to TA about how to have no required fields for this
+    elif (request.method == "POST"):
+        form = BookForm(request.POST, instance=theBook)
+        if (form.is_valid()):
+            form.save()
+            theBook = book.objects.get(pk=book_id)
+            book_dict = model_to_dict(theBook)
+            return JsonResponse({'status': True, 'resp': book_dict})
+        else:
+            return JsonResponse({'status': False, 'resp': 'invalid input'})
+
+    return JsonResponse({'status': False, 'resp': 'URL only handles GET and POST requests'})
 
 def createUser(request):
     if request.method == "POST":
         form = UserForm(data=request.POST)
         if(form.is_valid()):
-            userName = UserForm.userName_clean(form)
-            firstName = UserForm.firstName_clean(form)
-            lastName = UserForm.lastName_clean(form)
-            password = UserForm.password_clean(form)
-            p = user(user_name=userName, first_name=firstName, last_name=lastName, password=password)
-            p.save()
-            return render(request, 'index.html',{'message': "Account Created!!\n"})
-        else: return render(request, 'createUser.html', {'form':form})
-    form = UserForm()
-    return render(request, 'createUser.html', {'form':form})
-
+            form.save()
+            return JsonResponse({'status': True, 'resp' : 'user has been created.'})
+        else: return JsonResponse({'status': False, 'resp': 'invalid input'})
+    return JsonResponse({'status' : False, 'resp': 'This URL only handles POST requests'})
 
 def createBook(request):
     if request.method == "POST":
         form = BookForm(data=request.POST)
         if(form.is_valid()):
-            author = BookForm.author_clean(form)
-            title = BookForm.title_clean(form)
-            pub = BookForm.publisher_clean(form)
-            pub_date = BookForm.date_clean(form)
-            price = BookForm.price_clean(form)
-            isbn = BookForm.isbn_clean(form)
-            b = book(isbn_num=isbn, author=author, title=title, pub_date=pub_date, publisher=pub, price=price)
-            b.save()
-            return render(request, 'index.html',{'message': "Book Created!!\n"})
-        else: return render(request, 'createBook.html', {'form':form})
-    form = BookForm()
-    return render(request, 'createBook.html', {'form':form})
+            form.save()
+            return JsonResponse({'status': True, 'resp' : 'book has been created.'})
+        else: return JsonResponse({'status': False, 'resp': 'invalid input'})
+    return JsonResponse({'status' : False, 'resp': 'This URL only handles POST requests'})
 
 
 def removeUser(request, user_id):
     if user.objects.filter(id = user_id):
-        theUser = user.objects.filter(id = user_id)
-        theUser = theUser.get()
+        theUser = user.objects.get(pk = user_id)
         theUser.delete()
-        response_data = {}
-        response_data['result'] = '200'
-        response_data['message'] = 'Succesfully removed user'
-        return JsonResponse(response_data, safe = False)
+        return JsonResponse({'status': True, 'resp': 'User Deleted'}, safe = False)
     else:
-        response_data = {}
-        response_data['result'] = '404'
-        response_data['message'] = "Not Found: user item not found"
-        return JsonResponse(response_data, safe = False)
+        return JsonResponse({'status': False, 'resp': 'User Not Found'}, safe=False)
 
 
 def removeBook(request, book_id):
     if book.objects.filter(id = book_id):
-        theBook = book.objects.filter(id = book_id)
-        theBook = theBook.get()
+        theBook = book.objects.get(pk = book_id)
         theBook.delete()
-        response_data = {}
-        response_data['result'] = '200'
-        response_data['message'] = 'Succesfully removed book'
-        return JsonResponse(response_data, safe = False)
+        return JsonResponse({'status': True, 'resp': 'Book Deleted'}, safe = False)
     else:
-        response_data = {}
-        response_data['result'] = '404'
-        response_data['message'] = "Not Found: book item not found"
-        return JsonResponse(response_data, safe = False)
-
-
-def editUser(request, user_id):
-    theUser = user.objects.filter(id=user_id)
-    theUser = theUser.get()
-    json = {}
-    json['username'] = theUser.user_name
-    json['first name'] = theUser.first_name
-    json['last name'] = theUser.last_name
-    json['password'] = theUser.password
-    json['ID'] = theUser.id
-    form = UserForm()
-    if request.method == "POST":
-        form = UserForm(data=request.POST)
-        if(form.is_valid()):
-            theUser.user_name = UserForm.userName_clean(form)
-            theUser.first_name = UserForm.firstName_clean(form)
-            theUser.last_name = UserForm.lastName_clean(form)
-            theUser.password = UserForm.password_clean(form)
-            theUser.save()
-            return render(request, 'items.html', {'json': json, 'form':form, 'message':"user edited"})
-        else: return render(request, 'item.html', {'json': json, 'form':form, 'message':"edit the user below"})
-    form = UserForm()
-    return render(request, 'items.html', {'json': json, 'form':form, 'message':"edit the user below"})
-
-
-def editBook(request, book_id):
-    theBook = book.objects.filter(id = book_id)
-    theBook = theBook.get()
-    json = {}
-    json['title'] = theBook.title
-    json['author'] = theBook.author
-    json['publisher'] = theBook.publisher
-    json['publication date'] = theBook.pub_date
-    json['isbn'] = theBook.isbn_num
-    json['price'] = theBook.price
-    json['ID'] = theBook.id
-    form = BookForm()
-    if request.method == "POST":
-        form = BookForm(data=request.POST)
-        if(form.is_valid()):
-            theBook.author = BookForm.author_clean(form)
-            theBook.title = BookForm.title_clean(form)
-            theBook.pub = BookForm.publisher_clean(form)
-            theBook.pub_date = BookForm.date_clean(form)
-            theBook.price = BookForm.price_clean(form)
-            theBook.isbn_num = BookForm.isbn_clean(form)
-            theBook.save()
-            return render(request, 'items.html', {'json': json, 'form':form, 'message':"book edited"})
-        else: return render(request, 'item.html', {'json': json, 'form':form, 'message':"edit the book below"})
-    form = BookForm()
-    return render(request, 'items.html', {'json': json, 'form':form, 'message':"edit the book below"})
+        return JsonResponse({'status': False, 'resp': 'Book Not Found'}, safe=False)
 
 
 
