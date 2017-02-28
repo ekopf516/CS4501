89fe43263be4574bbba9b2f6a015cc6ab693fa91
John <jpm6zv@virginia.edu>
2017-02-28 15:53:36 -0500

    nothing changed

--- web/mainpage/views.py
+++ web/mainpage/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
-def index(request):
-    return render(
-        request,
-        'index.html',
-)
+# def index(request):
+#     req = urllib.request.Request('http://localhost:8001/user_display/')
+#     resp_json = urllib.request.urlopen(req).read().decode('utf-8')
+#     resp = json.loads(resp_json)
+#     return render(request, 'index.html',)
