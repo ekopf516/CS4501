1bb6a63d87abf70fce109dd5669591d135c37cc3
John <jpm6zv@virginia.edu>
2017-03-21 19:18:09 -0400

    handling no book exception

--- web/mainpage/views.py
+++ web/mainpage/views.py
@@ -34,6 +34,8 @@ def bookView(request, book_id):
         req = urllib.request.Request('http://exp-api:8000/book_display/' + book_id + '/')
         resp_json = urllib.request.urlopen(req).read().decode('utf-8')
         resp = json.loads(resp_json)
+        if (resp['resp']['status'] == False):
+            return render(request, 'noBook.html')
         resp = resp['resp']['resp']
         # return JsonResponse({'status': True, 'resp': resp})
         return render(request, 'book.html', resp)
