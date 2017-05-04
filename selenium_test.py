b8b685a4088ef7789c655e3e5160fe8c4f420598
aar0n-aguh0b <aaronaguhob@gmail.com>
2017-05-04 14:48:17 -0400

    modified selenium test file

--- selenium_test.py
+++ selenium_test.py
@@ -1,9 +1,12 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 driver = webdriver.Chrome('./chromedriver')
 driver.get('localhost:8000/home');
+assert "JEA and Books" in driver.title
 
 elem = driver.find_element_by_name('searchquery')
+elem.clear()
 elem.send_keys('Goats' + Keys.RETURN)
-
+assert "No results found." not in driver.page_source
 driver.quit()
